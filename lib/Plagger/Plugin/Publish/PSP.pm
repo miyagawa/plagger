8494ec04fbd6ea4a83978e4296ba5122f8405f78
Masayoshi Sekimura <sekimura@gmail.com>
2006-02-05 02:40:36 +0000

    fixed a bug overwriting the exiting output_file

--- lib/Plagger/Plugin/Publish/PSP.pm
+++ lib/Plagger/Plugin/Publish/PSP.pm
@@ -8,30 +8,26 @@ sub register {
     my($self, $context) = @_;
     $context->register_hook(
         $self,
-        'publish.notify' => \&notify,
+        'publish.notify'   => \&notify,
+        'publish.finalize' => \&finalize,
     );
 }
 
 sub notify {
     my($self, $context, $feed) = @_;
 
-    my @items = $feed->entries;
-    $self->store_items_as_html($context, $feed, \@items);
-}
-
-sub store_items_as_html {
-    my($self, $context, $feed, $items) = @_;
     $feed->{title} = $feed->{title} || '(no-title)';
-    my $body =  $self->templatize($context, $feed, $items);
-    $self->do_store_item($context, $feed, $body);
+    $context->log(warn => "Store $feed->{title}");
+    push @{ $self->{__feeds} }, $feed;
 }
 
-sub do_store_item {
-    my($self, $context, $feed, $body) = @_;
 
+sub finalize {
+    my($self, $context) = @_;
+
+    my $body= $self->templatize($context, $self->{__feeds});
     my $cfg = $self->conf;
     my $file= $cfg->{output_file};
-    $context->log(warn => "Store $feed->{title} to $file");
 
     open(FH, ">:utf8", $file) or die $!;
     print FH $body;
@@ -39,12 +35,10 @@ sub do_store_item {
 }
 
 sub templatize {
-    my($self, $context, $feed, $items) = @_;
+    my($self, $context, $feeds) = @_;
     my $tt = $context->template();
-    $tt->process('psp_notify.tt', {
-        feed => $feed,
-        items => $items,
-        cfg  => $self->conf,
+    $tt->process('psp.tt', {
+        feeds => $feeds,
         utf8 => sub { encode("utf-8", $_[0]) }
     }, \my $out) or die $tt->error;
     $out;
