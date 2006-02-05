f21daf1fd8ccadfe67eeb47a13fe765683ef1a58
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-02-05 22:19:33 +0000

    Added TLS support to Publish::Gmail. Fixes #9

--- lib/Plagger/Plugin/Publish/Gmail.pm
+++ lib/Plagger/Plugin/Publish/Gmail.pm
@@ -71,8 +71,16 @@ sub do_send_mail {
     );
 
     my $route = $cfg->{mailroute} || { via => 'smtp', host => 'localhost' };
-    my @args  = $route->{host} ? ($route->{host}) : ();
-    $msg->send($route->{via}, @args);
+    if ($route->{via} eq 'smtp_tls') {
+        $msg->send_by_smtp_tls(
+            $route->{host},
+            User     => $route->{username},
+            Password => $route->{password},
+        );
+    } else {
+        my @args  = $route->{host} ? ($route->{host}) : ();
+        $msg->send($route->{via}, @args);
+    }
 }
 
 sub htmlize {
@@ -100,5 +108,57 @@ sub templatize {
     }, \my $out) or $context->error($tt->error);
     $out;
 }
+
+# hack MIME::Lite to support TLS Authentication
+package MIME::Lite;
+
+sub send_by_smtp_tls {
+    my($self, @args) = @_;
+
+    ### We need the "From:" and "To:" headers to pass to the SMTP mailer:
+    my $hdr  = $self->fields();
+    my $from = $self->get('From');
+    my $to   = $self->get('To');
+
+    ### Sanity check:
+    defined($to) or Carp::croak "send_by_smtp_tls: missing 'To:' address\n";
+
+    ### Get the destinations as a simple array of addresses:
+    my @to_all = extract_addrs($to);
+    if ($MIME::Lite::AUTO_CC) {
+        foreach my $field (qw(Cc Bcc)) {
+            my $value = $self->get($field);
+            push @to_all, extract_addrs($value) if defined($value);
+        }
+    }
+
+    ### Create SMTP TLS client:
+    require Net::SMTP::TLS;
+    my $smtp = MIME::Lite::SMTP::TLS->new(@args)
+        or Carp::croak("Failed to connect to mail server: $!\n");
+    $smtp->mail($from);
+    $smtp->to(@to_all);
+    $smtp->data();
+
+    ### MIME::Lite can print() to anything with a print() method:
+    $self->print_for_smtp($smtp);
+    $smtp->dataend();
+    eval {
+        local $SIG{__WARN__} = sub { };
+        $smtp->quit;
+    };
+
+    # known error from Gmail SMTP
+    if ($@ && $@ !~ /An error occurred disconnecting from the mail server/) {
+        warn $@;
+    }
+
+    1;
+}
+
+package MIME::Lite::SMTP::TLS;
+use base qw( Net::SMTP::TLS );
+
+sub print { shift->datasend(@_) }
 
 1;
