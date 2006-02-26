c6db0e8deac613c36869b0308d67f3acb0c7693d
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-02-26 17:12:19 +0000

    Added DOC to FeedBurner permalink filter

--- lib/Plagger/Plugin/Filter/FeedBurnerPermalink.pm
+++ lib/Plagger/Plugin/Filter/FeedBurnerPermalink.pm
@@ -20,3 +20,37 @@ sub filter {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Filter::FeedBurnerPermalink - Fix FeedBurner's permalink
+
+=head1 SYNOPSIS
+
+  - module: Filter::FeedBurnerPermalink
+
+=head1 DESCRIPTION
+
+Entries in FeedBurner feeds contain links to feedburner's URL
+redirector and that breaks some plugins like social bookmarks
+integration.
+
+This plugin updates the C<< $entry->permalink >> with I<guid> value in
+FeedBurner's feed, so it actually points to the permalink, rather than
+redirector.
+
+Note that C<< $entry->link >> will still point to the redirector.
+
+=head1 AUTHOR
+
+Masahiro Nagano
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plagger>, L<http://www.feedburner.com/>
+
+=cut
