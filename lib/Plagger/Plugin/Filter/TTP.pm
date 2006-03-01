63e4a422b0c2ec024584bf5dbcfaf2afddc5fabe
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-03-01 09:26:07 +0000

    Filter::TTP: Added html_paranoia option to use HTML::Parser. Fixes #76

--- lib/Plagger/Plugin/Filter/TTP.pm
+++ lib/Plagger/Plugin/Filter/TTP.pm
@@ -1,6 +1,7 @@
 package Plagger::Plugin::Filter::TTP;
 use strict;
 use base qw( Plagger::Plugin );
+
 use URI::Find;
 
 sub register {
@@ -15,15 +16,53 @@ sub update {
     my($self, $context, $args) = @_;
     my $body = $args->{entry}->body;
 
+    my $count;
+    if ($self->conf->{html_paranoia}) {
+        ($count, $body) = $self->paranoia_rewrite($body);
+    } else {
+        ($count, $body) = $self->rewrite_ttp($body);
+    }
+
+    if ($count) {
+        $context->log(info => "Rewrite $count ttp:// link(s) to http://");
+    }
+
+    $args->{entry}->body($body);
+}
+
+sub rewrite_ttp {
+    my($self, $body) = @_;
+
     local @URI::ttp::ISA = qw(URI::http);
 
     my $finder = URI::Find->new(sub {
         my ($uri, $orig_uri) = @_;
         return ($uri->scheme eq 'ttp') ? qq{<a href="h$orig_uri">$orig_uri</a>} : $orig_uri;
     });
-    $finder->find(\$body);
 
-    $args->{entry}->body($body);
+    my $count = $finder->find(\$body);
+
+    ($count, $body);
+}
+
+sub paranoia_rewrite {
+    my($self, $body) = @_;
+    require HTML::Parser;
+
+    my($count, $output);
+
+    my $p = HTML::Parser->new(api_version => 3);
+    $p->handler( default => sub { $output .= $_[0] }, "text" );
+    $p->handler( text => sub {
+        my($c, $body) = $self->rewrite_ttp($_[0]);
+        $count  += $c;
+        $output .= $body;
+    }, "text");
+
+    $p->parse($body);
+    $p->eof;
+
+    ($count, $output);
 }
 
 1;
@@ -43,12 +82,25 @@ Plagger::Plugin::Filter::TTP - Replace ttp:// with http://
 This plugin replaces C<ttp://> with C<http://>. C<ttp://> is a widely
 adopted way of linking an URL without leaking a referer.
 
+=head1 CONFIG
+
+=over 4
+
+=item html_paranoia
+
+When set to 1, uses HTML::Parser to avoid replacing C<ttp://> inside
+HTML elements. Defaults to 0.
+
+=back
+
 =head1 AUTHOR
 
 Matsuno Tokuhiro
 
+Tatsuhiko Miyagawa
+
 =head1 SEE ALSO
 
-L<Plagger>
+L<Plagger>, L<HTML::Parser>
 
 =cut
