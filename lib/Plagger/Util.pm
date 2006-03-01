a43541c2f610f8b45b0493cc98332b1bfb5ee605
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-03-01 09:49:09 +0000

    decode entities in strip_html. Fixes #82

--- lib/Plagger/Util.pm
+++ lib/Plagger/Util.pm
@@ -3,10 +3,12 @@ use strict;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw( strip_html );
 
+use HTML::Entities;
+
 sub strip_html {
     my $html = shift;
     $html =~ s/<[^>]*>//g;
-    $html;
+    HTML::Entities::decode($html);
 }
 
 1;
