35074e48797f8eecde22e792d3e1fe6d9a6240c0
kazeburo <kazeburo@gmail.com>
2006-03-03 09:33:50 +0000

    added Plagger::Plugin::Filter::FloatingDateTime

---
+++ lib/Plagger/Plugin/Filter/FloatingDateTime.pm
@@ -0,0 +1,46 @@
+package Plagger::Plugin::Filter::FloatingDateTime;
+use strict;
+use base qw( Plagger::Plugin );
+
+sub register {
+    my($self, $context) = @_;
+    $context->register_hook(
+        $self,
+        'update.entry.fixup' => \&update,
+    );
+    $self->{tz} = $context->conf->{timezone} || 'local';
+}
+
+sub update {
+    my($self, $context, $args) = @_;
+    $args->{entry}->date and $args->{entry}->date->time_zone->is_floating and
+	$args->{entry}->date->set_time_zone($self->{tz});
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Filter::FloatingDateTime - fix floating timezone
+
+=head1 SYNOPSIS
+
+    - module: Filter::FloatingDateTime
+
+=head1 DESCRIPTION
+
+This plugin fixes a floating timezone.
+
+=head1 AUTHOR
+
+Masahiro Nagano
+
+=head1 SEE ALSO
+
+L<Plagger>, L<DateTime>
+
+=cut
+
+
