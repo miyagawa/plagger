fd63e63a79787975145300889aae8bf1a2270fe6
Casey West <casey@geeknest.com>
2006-03-05 01:28:33 +0000

    Default Planet Plugin now has support for members listing, optional

--- lib/Plagger/Plugin/Publish/Planet.pm
+++ lib/Plagger/Plugin/Publish/Planet.pm
@@ -23,7 +23,18 @@ sub add_feed {
         $context->error("Publish::Planet requires SmartFeed::All to run.");
     }
 
-    $self->_sanitize_entries($context, $feed);
+    $self->_sanitize_entries(
+        $context,
+        $feed,
+        HTML::Tidy->new,
+        HTML::Scrubber->new(
+            rules => [
+                style => 0,
+                script => 0,
+            ],
+            default => [ 1, { '*' => 1, style => 0 } ],
+        ),
+    );
 
     $self->_write_index(
         $context,
@@ -45,17 +56,19 @@ sub templatize {
     my $skin = $self->conf->{skin} || 'default';
 
     $tt->process("$skin/template/index.tt", {
+        %{ $self->conf->{template} },
         feed  => $feed,
+        members => $self->_get_members($context, $feed),
     }, \my $out) or $context->error($tt->error);
     $out;
 }
 
 sub _sanitize_entries {
-    my ($self, $context, $feed) = shift;
+    my ($self, $context, $feed, $tidy, $scrubber) = @_;
     
-    foreach my $entry (@{$feed->{entries}}) {
-        $entry->{body} = HTML::Tidy->new->clean($entry->{body});
-        $entry->{body} = HTML::Scrubber->new->scrub($entry->{body});
+    foreach my $entry ($feed->entries) {
+        $entry->{body} = $tidy->clean($entry->{body});
+        $entry->{body} = $scrubber->scrub($entry->{body});
     }
 }
 
@@ -76,6 +89,22 @@ sub _apply_skin {
         join('/', $self->assets_dir, $skin_name, 'static'),
         $output_dir,
     ) or $context->error("rcopy: $!");
+}
+
+sub _get_members {
+    my ($self, $context, $feed) = @_;
+    
+    my %members;
+    foreach my $entry ($feed->entries) {
+        unless ($members{$entry->source->id}) {
+            $members{$entry->source->id} = {
+                title => $entry->source->title,
+                link  => $entry->source->link,
+            };
+        }
+    }
+    
+    return [ sort { $a->{title} cmp $b->{title} } values %members ];
 }
 
 1;
