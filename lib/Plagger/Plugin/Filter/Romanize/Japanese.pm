9ee456000f195bd3a21b3fa3439df6f2cc333213
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-03-31 10:43:55 +0000

    fix Kakasi API

--- lib/Plagger/Plugin/Filter/Romanize/Japanese.pm
+++ lib/Plagger/Plugin/Filter/Romanize/Japanese.pm
@@ -8,8 +8,10 @@ use Text::Kakasi;
 
 sub romanize {
     my($self, $text) = @_;
-    $self->{kakasi} ||= Text::Kakasi->new(qw/-Ha -Ka -Ja -Ea -ka -iutf8/);
-    $self->{kakasi}->get( encode("utf-8", $text) );
+    $self->{wakati} ||= Text::Kakasi->new(qw/-w -iutf8/);
+    $self->{roman}  ||= Text::Kakasi->new(qw/-Ha -Ka -Ja -Ea -ka -iutf8/);
+    my @wakati = split /\s+/, $self->{wakati}->get( encode("utf-8", $text) );
+    [ map $self->{roman}->get($_), @wakati ];
 }
 
 1;
