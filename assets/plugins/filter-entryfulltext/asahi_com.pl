660845fcbc202cc37338a4048064f3e89aacc08c
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-04-02 04:13:43 +0000

    YAMLified asahi.com sample

--- assets/plugins/filter-entryfulltext/asahi_com.pl
+++
@@ -1,9 +0,0 @@
-sub handle {
-    my($self, $args) = @_;
-    $args->{entry}->link =~ qr!^http://www\.asahi\.com/!;
-}
-
-sub extract {
-    my($self, $args) = @_;
-    ( $args->{content} =~ /<!-- Start of Kiji -->(.*)<!-- End of Kiji -->/s )[0];
-}
