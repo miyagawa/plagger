2647059e9fe250b71da7bc1e92a386192b4cbf59
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-04-04 09:57:33 +0000

    add HatenaDiaryKeywordUnlink filter

---
+++ lib/Plagger/Plugin/Filter/HatenaDiaryKeywordUnlink.pm
@@ -0,0 +1,52 @@
+package Plagger::Plugin::Filter::HatenaDiaryKeywordUnlink;
+use strict;
+use base qw( Plagger::Plugin );
+
+sub register {
+    my($self, $context) = @_;
+    $context->register_hook(
+        $self,
+        'update.entry.fixup' => \&update,
+    );
+}
+
+sub update {
+    my($self, $context, $args) = @_;
+    my $body = $args->{entry}->body;
+
+    my $count = $body =~ s!<a class="keyword" href="http://(?:d|[\w\-]+\.g)\.hatena\.ne\.jp/keyword/.*?">(.*?)</a>!$1!g;
+
+    if ($count) {
+        $context->log(info => "Stripped $count links to Hatena Diary Keywords");
+    }
+
+    $args->{entry}->body($body);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Filter::HatenaDiaryKeywordUnlink - Strip Hatena Diary keyword links from fulltext feeds
+
+=head1 SYNOPSIS
+
+  - module: Filter::HatenaDiaryKeywordUnlink
+
+=head1 DESCRIPTION
+
+This plugin strips link to Hatena Diary keyword links in feeds. By
+default Hatena Diary feeds don't contain links to keywords, but with
+Filter::EntryFullText plugin it might contain them.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plagger>
+
+=cut
