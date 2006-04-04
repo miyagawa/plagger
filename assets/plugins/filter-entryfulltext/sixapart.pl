a37cb321a456f5481d8dc9f4d0970c172527572a
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-04-04 10:23:39 +0000

    update Six Apart std template

--- assets/plugins/filter-entryfulltext/sixapart.pl
+++ assets/plugins/filter-entryfulltext/sixapart.pl
@@ -5,8 +5,8 @@ sub handle {
 
 sub extract {
     my($self, $args) = @_;
-    my $body = ($args->{content} =~ m!<div class="entry-body-text">(.*?)</div>!s)[0];
-    if ($body && ($args->{content} =~ m!<div class="entry-more-text">(.*?)</div>!s)[0]) {
+    my $body = ($args->{content} =~ m!<div class="entry-body(?:-text)?">(.*?)</div>!s)[0];
+    if ($body && ($args->{content} =~ m!<div class="entry-more(?:-text)?">(.*?)</div>!s)[0]) {
         $body .= $1;
     }
     $body;
