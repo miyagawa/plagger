bb5775523eb1ff65eec608bf3a7169f46105927a
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-04-05 09:03:46 +0000

    * hatena_diary.pl now supports Hatena Group as well.  * HatenaDiaryKeywordUnlink: strip links to other group keywords

--- assets/plugins/filter-entryfulltext/hatena_diary.pl
+++ assets/plugins/filter-entryfulltext/hatena_diary.pl
@@ -1,17 +1,21 @@
 sub handle {
     my($self, $args) = @_;
-    $args->{entry}->link =~ qr!^http://d\.hatena\.ne\.jp/!;
+    $args->{entry}->link =~ qr!^http://(?:[\w\-]+\.g|d)\.hatena\.ne\.jp/!;
 }
 
 sub extract {
     my($self, $args) = @_;
-    my $name     = ( $args->{entry}->link =~ /\#([\w\-]+)$/ )[0];
-    my $day_only = $args->{entry}->link =~ qr!^http://d\.hatena\.ne\.jp/[\w\-]+/\d+/?$!;
+    my $path = URI->new($args->{entry}->link)->path;
+
+    my $name     = ( $path =~ /\#([\w\-]+)$/ )[0];
+    my $day_only = $path =~ m!^/[\w\-]+/\d+/?$!;
 
     my $match =
          $name     ? qq!<h3><a href=".*?" name="$name">.*?</h3>(.*?)</div>! :
          $day_only ? qq!<div class="section">(.*?)</div>! :
                      qq!</h3>(.*?)</div>!;
+
+    warn $match;
 
     if ( $args->{content} =~ /$match/s ){
         my $body = $1;
