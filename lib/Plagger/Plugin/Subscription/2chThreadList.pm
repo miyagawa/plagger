d93483a9ed340b76b1fa80718d331c1c24cd7ada
youpy <youpy@buycheapviagraonlinenow.com>
2006-04-16 13:49:33 +0000

    Subscription::2chThreadList now can load multiple urls.

--- lib/Plagger/Plugin/Subscription/2chThreadList.pm
+++ lib/Plagger/Plugin/Subscription/2chThreadList.pm
@@ -17,18 +17,20 @@ sub register {
 sub load {
     my($self, $context) = @_;
 
-    my $threadlist_uri = $self->conf->{url}
+    my $threadlists = ref($self->conf->{url}) ? $self->conf->{url} : [ $self->conf->{url} ]
         or $context->error('ThreadList url is missing');
 
-    my $remote = XML::Feed->parse(URI->new($threadlist_uri)) or $context->error("feed parse error $threadlist_uri");
-    for my $r ($remote->entries) {
-        $context->log(info => "thread: ". $r->link);
-
-        my $feed = Plagger::Feed->new;
-        $feed->url($r->link);
-        $feed->link($r->link);
-        $feed->title($r->title);
-        $context->subscription->add($feed);
+    for my $threadlist (@$threadlists) {
+	my $remote = XML::Feed->parse(URI->new($threadlist)) or $context->error("feed parse error $threadlist");
+	for my $r ($remote->entries) {
+	    $context->log(info => "thread: ". $r->link);
+	    
+	    my $feed = Plagger::Feed->new;
+	    $feed->url($r->link);
+	    $feed->link($r->link);
+	    $feed->title($r->title);
+	    $context->subscription->add($feed);
+	}
     }
 }
 
