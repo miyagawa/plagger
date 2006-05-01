5deaa908916bd42d29bbcdb2922fe2c005d795ac
Yuichi Tateno <hotchpotch@gmail.com>
2006-05-01 11:09:41 +0000

    add new filter plugin HatenaKeywordNewsokuTag

---
+++ lib/Plagger/Plugin/Filter/HatenaKeywordNewsokuTag.pm
@@ -0,0 +1,53 @@
+package Plagger::Plugin::Filter::HatenaKeywordNewsokuTag;
+use strict;
+use base qw( Plagger::Plugin );
+use Hatena::Keyword;
+
+sub register {
+    my($self, $context) = @_;
+    $context->register_hook(
+        $self,
+        'update.entry.fixup' => \&update,
+    );
+}
+
+sub update {
+    my($self, $context, $args) = @_;
+    my $title = $args->{entry}->title;
+    my $body = $args->{entry}->body;
+    Encode::_utf8_off($body); # Hatena::Keyword's Bug?
+    my $keywords = Hatena::Keyword->extract($body);
+    my @terms = sort { $a->refcount <=> $b->refcount } @$keywords;
+
+    for my $term (@terms) {
+        $args->{entry}->add_tag($term);
+    }
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Filter::HatenaKeywordNewsokuTag - Hatena::Keyword API for auto-tagging
+
+=head1 SYNOPSIS
+
+  - module: Filter::HatenaKeywordNewsokuTag
+  - module: Filter::2chNewsokuTitle
+
+=head1 DESCRIPTION
+
+Hatena::Keyword API for auto-tagging
+
+=head1 AUTHOR
+
+Yuichi Tateno (id:secondlife)
+
+=head1 SEE ALSO
+
+L<Plagger>
+L<Hatena::Keyword>
+
+=cut
