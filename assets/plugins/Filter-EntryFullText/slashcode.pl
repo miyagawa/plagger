a1bb5f988a7475d79c41e3dd1df2c029da46580c
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-05-16 16:54:25 +0000

    added slashcode.pl from http://blog.nomadscafe.jp/archives/000666.html

---
+++ assets/plugins/Filter-EntryFullText/slashcode.pl
@@ -0,0 +1,14 @@
+sub handle {
+    my($self, $args) = @_;
+    $args->{entry}->permalink =~ m!article\.pl\?sid=\d\d/\d\d/\d\d/\d+!;
+}
+
+sub extract {
+    my($self, $args) = @_;
+
+    my $body = ($args->{content} =~ m!<div class="intro(?:text)?">(.*?)</div>!s)[0];
+    if ($body && ($args->{content} =~ m!<div class="(?:bodytext|full)?">(.*?)</div>!s)[0]) {
+        $body .= $1;
+    }
+    $body;
+}
