5301d39d5bc0d4ed2c0160c2dfe83a7650c92685
youpy <youpy@buycheapviagraonlinenow.com>
2006-05-18 16:57:54 +0000

    Refactored

--- lib/Plagger/Plugin/Subscription/XOXO.pm
+++ lib/Plagger/Plugin/Subscription/XOXO.pm
@@ -1,52 +1,12 @@
 package Plagger::Plugin::Subscription::XOXO;
 use strict;
-use base qw( Plagger::Plugin );
-
-use HTML::TreeBuilder::XPath;
-use Plagger::Util;
-use URI;
-
-sub register {
-    my($self, $context) = @_;
-
-    $context->register_hook(
-        $self,
-        'subscription.load' => \&load,
-    );
-}
+use base qw( Plagger::Plugin::Subscription::XPath );
 
 sub load {
     my($self, $context) = @_;
-    my $uri = URI->new($self->conf->{url})
-        or $context->error("config 'url' is missing");
 
-    $self->load_xoxo($context, $uri);
-}
-
-sub load_xoxo {
-    my($self, $context, $uri) = @_;
-
-    my $xhtml = Plagger::Util::load_uri($uri, $self);
-    my $tree = HTML::TreeBuilder::XPath->new;
-    $tree->parse($xhtml);
-    $tree->eof;
-
-    $self->find_xoxo($tree);
-}
-
-sub find_xoxo {
-    my($self, $tree) = @_;
-
-    for my $child ($tree->findnodes('//ul[@class="xoxo" or @class="subscriptionlist"]//a')) {
-        my $href  = $child->attr('href') or next;
-        my $title = $child->attr('title') || $child->as_text;
-
-        my $feed = Plagger::Feed->new;
-        $feed->url($href);
-        $feed->title($title);
-
-        Plagger->context->subscription->add($feed);
-    }
+    $self->conf->{xpath} = '//ul[@class="xoxo" or @class="subscriptionlist"]//a';
+    $self->SUPER::load($context);
 }
 
 1;
