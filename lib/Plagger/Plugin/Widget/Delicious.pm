277fac1e338c585af4751765c743e79b48e48561
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-05-19 08:27:48 +0000

    Widget::Delicious: code fix and POD update

--- lib/Plagger/Plugin/Widget/Delicious.pm
+++ lib/Plagger/Plugin/Widget/Delicious.pm
@@ -23,10 +23,11 @@ sub html {
     my($self, $entry) = @_;
     my $uri = URI->new('http://del.icio.us/' . $self->conf->{username});
     my %query;
-    $query{'url'}  = $entry->permalink;
-    $query{'description'} = encode('utf-8', $entry->title);
-    $query{'tags'} = $self->conf->{tags} if $self->conf->{tags};
-    $query{'jump'} = 'doclose' if $self->conf->{one_click_post} == 1;
+    $query{url}         = $entry->permalink;
+    $query{description} = encode('utf-8', $entry->title);
+    $query{tags}        = $self->conf->{tags} if $self->conf->{tags};
+    $query{jump}        = 'doclose' if $self->conf->{one_click_post};
+
     $uri->query_form(%query);
 
     my $url = HTML::Entities::encode($uri->as_string);
@@ -34,3 +35,53 @@ sub html {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Widget::Delicious - Widget to post to del.icio.us
+
+=head1 SYNOPSIS
+
+   module: Widget::Delicious
+   config:
+     username: miyagawa
+
+=head1 DESCRIPTION
+
+This plugin creates a widget to post to del.icio.us in the Publish
+modules output.
+
+=head1 CONFIG
+
+=over 4
+
+=item username
+
+Your del.icio.us username. Required.
+
+=item tags
+
+Preset tags to tag the post.
+
+  tags: foo bar
+
+will set I<foo bar> as a predefined tag to use. Optional.
+
+=item one_click_post
+
+Flag to indicate that clicking the widget will automatically post the
+item, without showing the form. Defaults to 0. (Optional)
+
+=back
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plagger>, L<http://del.icio.us/>
+
+=cut
