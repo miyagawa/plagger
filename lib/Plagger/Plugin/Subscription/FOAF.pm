d1f8e208af5b4715a77ca82dd1cc2ba22e26473a
wolverian <@>
2006-05-30 03:16:33 +0000

    Add Subscription::FOAF and a basic test. Subscribe to your friends' blogs :)

---
+++ lib/Plagger/Plugin/Subscription/FOAF.pm
@@ -0,0 +1,77 @@
+package Plagger::Plugin::Subscription::FOAF;
+
+use strict;
+use warnings;
+
+use base qw(Plagger::Plugin);
+use Plagger::Util qw(load_uri);
+
+use XML::FOAF;
+
+sub register {
+    my ( $self, $context ) = @_;
+
+    $context->register_hook( $self, 'subscription.load' => \&load );
+}
+
+sub load {
+    my ( $self, $context ) = @_;
+
+    my $uri = URI->new( $self->conf->{url} )
+      or $context->error("config 'url' is missing");
+
+    $self->load_foaf( $context, $uri );
+}
+
+sub load_foaf {
+    my ( $self, $context, $uri ) = @_;
+
+    my $content = load_uri $uri;
+
+    my $person = eval { XML::FOAF->new( \$content, $uri )->person };
+
+    unless ($person) {
+        $context->log( debug => XML::FOAF->errstr );
+        return;
+    }
+
+    for my $friend ( @{ $person->knows } ) {
+        my $feed = Plagger::Feed->new;
+
+        $feed->url( $friend->weblog or next );
+
+        $context->subscription->add($feed);
+    }
+
+    return 1;
+    
+}
+
+1;
+
+=head1 NAME
+
+Plagger::Plugin::Subscription::FOAF - Simple subscription of friends' blogs
+
+=head1 SYNOPSIS
+
+    -   module: Subscription::FOAF
+        config:
+            url: http://user.livejournal.com/data/foaf
+
+=head1 DESCRIPTION
+
+This module subscribes to your friends' blogs in a FOAF file.
+
+=head1 AUTHOR
+
+Ilmari Vacklin <ilmari.vacklin@helsinki.fi>
+
+=head1 SEE ALSO
+
+L<Plagger>, L<http://xmlns.com/foaf/0.1/>
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
