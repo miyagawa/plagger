4b024a4586b0aa5eeed56b59ea360e487809089e
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-06-02 10:07:12 +0000

    refactored CustomFeed::Flickr  * Enclosure support  * Use cache for speed up

--- lib/Plagger/Plugin/CustomFeed/FlickrSearch.pm
+++ lib/Plagger/Plugin/CustomFeed/FlickrSearch.pm
@@ -6,6 +6,7 @@ use base qw( Plagger::Plugin );
 use Flickr::API;
 use XML::LibXML;
 use DateTime::Format::Epoch;
+use Plagger::Enclosure;
 
 sub register {
     my($self, $context) = @_;
@@ -29,15 +30,26 @@ sub aggregate {
 
     my $feed = Plagger::Feed->new;
     $feed->type('flickr.search');
-    $feed->title("Flickr Search");
-    $feed->id('flickr:search');
+    $feed->title("Flickr Search"); # xxx
+    $feed->id('flickr:search'); # xxx
 
     my $flickr = Flickr::API->new({key => $self->conf->{api_key}});
-    my $search = $flickr->execute_method('flickr.photos.search' => $self->conf);
+    my $method = $self->conf->{method} || 'flickr.photos.search';
+
+    my $params = $self->conf->{params} || {};
+    $params->{per_page} ||= 20;
+
+    $context->log(info => "calling $method on Flickr API");
+    my $search = $self->call_method(
+        $flickr,
+        $method,
+        $params,
+        60 * 60,
+    );
 
     my $parser = XML::LibXML->new;
 
-    $context->error("flickr.photos.search failed: $search->{error_text}")
+    $context->error("$method failed: $search->{error_text}")
       unless $search->{success};
     my $search_doc = $parser->parse_string($search->{_content});
 
@@ -51,42 +63,91 @@ sub aggregate {
 
 sub _create_entry {
     my ($self, $context, $flickr, $parser, $search_photo) = @_;
-    my $sizes = $flickr->execute_method('flickr.photos.getSizes', {
-        photo_id => $search_photo->findvalue('@id'),
-    });
-    next unless $sizes->{success};
-    
-    my $sizes_doc = $parser->parse_string($sizes->{_content});
-    my $size      = $self->conf->{size} || 'Square';
-    my $image_src =
-      $sizes_doc->findvalue(qq[/rsp/sizes/size[\@label='$size']/\@url]);
 
-    my $info = $flickr->execute_method('flickr.photos.getInfo', {
-        photo_id => $search_photo->findvalue('@id'),
-    });
+    my $photo_id  = $search_photo->findvalue('@id');
+    my $server_id = $search_photo->findvalue('@server');
+    my $secret    = $search_photo->findvalue('@secret');
+
+    my $size      = $self->conf->{size} || 'm';
+    my $thumb_src = sprintf "http://static.flickr.com/%s/%s_%s_t.jpg",
+        $server_id, $photo_id, $secret;
+
+    $context->log(info => "calling flickr.photos.getInfo on $photo_id");
+    my $info = $self->call_method(
+        $flickr,
+        'flickr.photos.getInfo',
+        { photo_id => $photo_id },
+        60 * 60,
+    );
     next unless $info->{success};
-    
+
     my $info_doc = $parser->parse_string($info->{_content});
     my $link     = $info_doc->findvalue(q[/rsp/photo/urls/url[@type='photopage']]);
-    my $author   = $info_doc->findvalue(q[/rsp/photo/owner/@realname]);
-       $author   = $info_doc->findvalue(q[/rsp/photo/owner/@username]) unless $author;
+    my $author   = $info_doc->findvalue(q[/rsp/photo/owner/@realname])
+                || $info_doc->findvalue(q[/rsp/photo/owner/@username]);
     my $title    = $info_doc->findvalue(q[/rsp/photo/title]);
     my $date     = $info_doc->findvalue(q[/rsp/photo/dates/@posted]);
+    my $format   = $info_doc->findvalue(q[/rsp/photo/@originalformat]) || 'jpg';
+    my $desc     = $info_doc->findvalue(q[/rsp/photo/description]);
+    my @tags     = map $_->textContent, $info_doc->findnodes('/rsp/photo/tags/tag');
 
-    my $description = $context->templatize($self, 'entry-description.tt', {
-        image_src => $image_src,
-        title     => $title,
-        link      => $link,
-    });
+    my $original = sprintf "http://static.flickr.com/%s/%s_%s_o.%s",
+        $server_id, $photo_id, $secret, $format;
     my $epoch = DateTime->from_epoch(epoch => 0, time_zone => '+0000');
 
     my $entry = Plagger::Entry->new;
     $entry->title($title);
     $entry->link($link);
-    $entry->body($description);
+    $entry->author($author);
+    $entry->body($desc);
     $entry->date(Plagger::Date->parse('Epoch::Unix', $date));
+    $entry->add_tag($_) for @tags;
+    $entry->icon({ url => $thumb_src });
+
+    my $enclosure = Plagger::Enclosure->new;
+    $enclosure->url($original);
+    $enclosure->auto_set_type;
+    $entry->add_enclosure($enclosure);
 
     return $entry;
 }
 
+sub call_method {
+    my($self, $flickr, $method, $param, $cache) = @_;
+
+    my $cache_key = "$method:" . join("|", map "$_=$param->{$_}", sort keys %$param);
+    $self->cache->get_callback(
+        $cache_key,
+        sub { $flickr->execute_method($method, $param) },
+        $cache,
+    );
+}
+
 1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::CustomFeed::FlickrSearch - Flickr API as Custom Feed
+
+=head1 SYNOPSIS
+
+  - module: CustomFeed::FlickrSearch
+    config:
+     api_key: YOUR-FLICKR-APIKEY
+     method: flickr.photos.search
+     params:
+       tags: plagger
+
+=head1 AUTHOR
+
+Casey West
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plagger>, L<http://www.flickr.com/>, L<Flickr::API>
+
+=cut
