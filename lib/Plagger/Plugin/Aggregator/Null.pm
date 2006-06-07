09a8049327738513549941a802b77eb62a4b8edb
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-06-07 09:30:04 +0000

    new plugin Aggregator::Null

---
+++ lib/Plagger/Plugin/Aggregator/Null.pm
@@ -0,0 +1,44 @@
+package Plagger::Plugin::Aggregator::Null;
+use strict;
+use base qw( Plagger::Plugin );
+
+sub register {
+    my($self, $context) = @_;
+    $context->register_hook(
+        $self,
+        'customfeed.handle'  => \&aggregate,
+    );
+}
+
+sub aggregate {
+    my($self, $context, $args) = @_;
+    $context->update->add($feed);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Aggregator::Null - Aggregator that doesn't do anything
+
+=head1 SYNOPSIS
+
+  - module: Aggregator::Null
+
+=head1 DESCRIPTION
+
+This plugin implements Plagger Aggregator but it doesn't do anything
+useful. It could be only useful when you want to just pass subscribed
+feed to Publish/Notify plugins, or inisde test scripts.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plagger>
+
+=cut
