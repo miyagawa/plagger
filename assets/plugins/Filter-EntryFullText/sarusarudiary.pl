d73fc759e1e2a02f853f123b8807874e3e4104a7
otsune <otsune@gmail.com>
2006-06-08 08:01:42 +0000

    add http://www.diary.ne.jp/

---
+++ assets/plugins/Filter-EntryFullText/sarusarudiary.pl
@@ -0,0 +1,18 @@
+# author: Masafumi Otsune
+sub handle {
+    my($self, $args) = @_;
+    $args->{entry}->link =~ qr!^http://www\d?\.diary\.ne\.jp/user/!;
+}
+
+sub extract {
+    my($self, $args) = @_;
+
+    my $data;
+    my $fragment = ( $args->{entry}->link =~ /#(\d+)$/ )[0];
+    $data->{date} = Plagger::Date->from_epoch(epoch => $fragment);
+    my $match = qq!<a name="$fragment">.*?</tr></table>(.*?)<table border=0!;
+    if ( $args->{content} =~ /$match/s ){
+        $data->{body} = "<div>$1</div>";
+    }
+    return $data;
+}
