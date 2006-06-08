bc20e13ba9681805a2e02ea5e9a831d9819bea23
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-06-08 13:39:27 +0000

    Publish::JavaScript: code tweak and docs

--- lib/Plagger/Plugin/Publish/JavaScript.pm
+++ lib/Plagger/Plugin/Publish/JavaScript.pm
@@ -13,16 +13,21 @@ sub register {
     );
 }
 
-sub feed {
-    my($self, $context, $args) = @_;
+sub init {
+    my $self = shift;
+    $self->SUPER::init(@_);
 
     my $dir = $self->conf->{dir};
     unless (-e $dir && -d _) {
-        mkdir $dir, 0755 or $context->error("mkdir $dir: $!");
+        mkdir $dir, 0755 or Plagger->context->error("mkdir $dir: $!");
     }
+}
 
-    my $file = $self->gen_filename($args->{feed});
-    my $path = File::Spec->catfile($dir, $file);
+sub feed {
+    my($self, $context, $args) = @_;
+
+    my $file = $self->gen_filename($args->{feed}, $self->conf->{filename} || '%i.js');
+    my $path = File::Spec->catfile($self->conf->{dir}, $file);
     $context->log(info => "writing output to $path");
 
     my $body = $self->templatize($context, $args->{feed});
@@ -42,9 +47,8 @@ my %formats = (
 my $format_re = qr/%(u|l|t|i)/;
 
 sub gen_filename {
-    my($self, $feed) = @_;
+    my($self, $feed, $file) = @_;
 
-    my $file = $self->conf->{filename};
     $file =~ s{$format_re}{
         $self->safe_filename($formats{$1}->($feed))
     }egx;
@@ -69,3 +73,67 @@ sub templatize {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Publish::JavaScript - publish links to entries as JavaScript
+
+=head1 SYNOPSIS
+
+  - module: Publish::JavaScript
+    config:
+      dir: /path/to/www/js
+      filename: %t.js
+
+=head1 DESCRIPTION
+
+This plugin publishes links to feed entries as an HTML embedable
+JavaScript file. The JS file contains document.write() calls, and can
+be easily included in any HTML page using:
+
+  <script src="/path/to/file.js"></script>
+
+in any place, like Blog sidebar widgets.
+
+The HTML emitted by the JavaScript code has exactly the same structure
+with Movable Type's standard sidebar module, so you can easily style
+using CSS.
+
+=head1 CONFIG
+
+=over 4
+
+=item dir
+
+Directory to save JS files in.
+
+=item filename
+
+Filename to be used to create JS files. It defaults to C<%i.js>, but
+could be configured using the following formats like strftime:
+
+=over 8
+
+=item * %u url
+
+=item * %l link
+
+=item * %t title
+
+=item * %i id
+
+=back
+
+=back
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plagger>, L<Plagger::Plugin::Publish::MTWidget>
+
+=cut
