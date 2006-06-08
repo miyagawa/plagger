b54cf97d54aaf7458d7f70152225ccce7824609b
otsune <otsune@gmail.com>
2006-06-08 13:39:27 +0000

    add http://memn0ck.com/ , http://yaplog.jp/

--- assets/plugins/Filter-EntryFullText/sarusarudiary.pl
+++ assets/plugins/Filter-EntryFullText/sarusarudiary.pl
@@ -8,7 +8,7 @@ sub extract {
     my($self, $args) = @_;
 
     my $data;
-    my $fragment = ( $args->{entry}->link =~ /#(\d+)$/ )[0];
+    my $fragment = ( $args->{entry}->link =~ /\#(\d+)$/ )[0];
     $data->{date} = Plagger::Date->from_epoch(epoch => $fragment);
     my $match = qq!<a name="$fragment">.*?</tr></table>(.*?)<table border=0!;
     if ( $args->{content} =~ /$match/s ){
