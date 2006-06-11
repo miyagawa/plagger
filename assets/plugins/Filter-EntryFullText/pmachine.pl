1a35827484515e5d9d16c25d606f4b0e4ce74169
otsune <otsune@gmail.com>
2006-06-11 01:17:02 +0000

    add pMachine ExpressionEngine http://www.pmachine.com/

---
+++ assets/plugins/Filter-EntryFullText/pmachine.pl
@@ -0,0 +1,14 @@
+# author: Masafumi Otsune
+sub handle {
+    my($self, $args) = @_;
+    $args->{content} =~ m!Powered by <a href="?http://www\.pmachine\.com/!si;
+}
+
+sub extract {
+    my($self, $args) = @_;
+    if ($args->{content} =~ m!<h2 class="title">(?:.*?)</h2>(?:</a>)?(.*?)<!--\n<rdf:RDF!s){
+        my $body = $1;
+        return "<div>$body</div>";
+    }
+    return;
+}
