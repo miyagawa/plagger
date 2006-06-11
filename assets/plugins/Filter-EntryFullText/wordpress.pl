753a520481cef7143fbacf3b85e96d8dffeb71b3
otsune <otsune@gmail.com>
2006-06-11 19:13:19 +0000

    update regexp

---
+++ assets/plugins/Filter-EntryFullText/wordpress.pl
@@ -0,0 +1,15 @@
+# author: Masafumi Otsune
+sub handle {
+    my($self, $args) = @_;
+    return $args->{content} =~ m!<meta name="generator" content="WordPress!
+               or 
+           $args->{content} =~ m!<a href="http://(?:www\.)?wordpress\.(?:xwd\.jp|org)/" title="Powered by WordPress.*?">!si;
+}
+
+sub extract {
+    my($self, $args) = @_;
+    if ($args->{content} =~ m#(<div class="storycontent">.*?)<div class="feedback">#){
+        return $1;
+    }
+    return;
+}
