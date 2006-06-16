2f523804bf35e582c6f07d9402c62ef8b9743fbc
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-06-16 06:04:12 +0000

    * New plugin Subscription::Bookmarks (and its IE subclass) to read IE favorites.

---
+++ lib/Plagger/Plugin/Subscription/Bookmarks/InternetExplorer.pm
@@ -0,0 +1,35 @@
+package Plagger::Plugin::Subscription::Bookmarks::InternetExplorer;
+use base qw( Plagger::Plugin::Subscription::Bookmarks );
+
+use Encode;
+use File::Basename qw( basename );
+use Win32::IEFavorites;
+use Win32::Locale;
+
+use URI;
+
+sub load {
+    my($self, $context) = @_;
+
+    my @items = Win32::IEFavorites->find(); # TODO: support expression?
+    for my $item (@items) {
+        my $url = URI->new( $item->url );
+        next if $url->scheme !~ /^http/;
+
+        my $language = Win32::Locale::get_language();
+        my $fs_encoding = $lanuage eq 'ja' ? "cp932" : "latin-1"; # xxx utf-8?
+
+        my $title = basename($item->path);
+        $title =~ s/\.url$//;
+        $title = decode($fs_encoding, $title);
+        
+        my $feed = Plagger::Feed->new;
+        $feed->url($item->url);
+        $feed->title($title);
+        # TODO: add favico?
+        
+        $context->subscription->add($feed);
+    }
+}
+
+1;
