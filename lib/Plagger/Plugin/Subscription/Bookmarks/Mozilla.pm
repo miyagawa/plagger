076bff8496d17e6b96788f81e2f3202a546f1040
youpy <youpy@buycheapviagraonlinenow.com>
2006-06-18 06:42:56 +0000

    add Plugin::Subscription::Bookmarks::Mozilla

---
+++ lib/Plagger/Plugin/Subscription/Bookmarks/Mozilla.pm
@@ -0,0 +1,13 @@
+package Plagger::Plugin::Subscription::Bookmarks::Mozilla;
+use base qw( Plagger::Plugin::Subscription::XPath );
+
+use URI::file;
+
+sub load {
+    my($self, $context) = @_;
+
+    $self->conf->{url} = URI::file->new($self->conf->{path});
+    $self->SUPER::load($context);
+}
+    
+1;
