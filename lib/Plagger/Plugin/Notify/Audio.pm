1ac6148a0d16a6e67d933ef3a17b3becd37fb827
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-08-19 08:04:09 +0000

    Notify::Audio: work with enclosures

--- lib/Plagger/Plugin/Notify/Audio.pm
+++ lib/Plagger/Plugin/Notify/Audio.pm
@@ -19,22 +19,36 @@ sub register {
     my($self, $context) = @_;
     $context->register_hook(
         $self,
-        'publish.feed' => \&update,
+        'publish.entry.fixup' => \&update,
         'publish.finalize' => \&finalize,
     );
+    $self->{enclosures} = [ ];
     $self->{count} = 0;
 }
 
 sub update {
     my($self, $context, $args) = @_;
-    $self->{count}++ if $args->{feed}->count;
+
+    if ($self->conf->{play_enclosures}) {
+        push @{$self->{enclosures}}, grep $_->local_path, $args->{entry}->enclosures;
+    } else {
+        $self->{count}++;
+    }
 }
 
 sub finalize {
     my($self, $context, $args) = @_;
-    $self->play($self->conf->{filename}) if $self->{count};
-}
 
+    if ($self->{count}) {
+        $self->log(info => "Play $self->conf->{filename}");
+        return $self->play($self->conf->{filename});
+    }
+
+    for my $enclosures (@{$self->{enclosures}}) {
+        $self->log(info => "Play " . $enclosure->local_path);
+        $self->play($enclosure->local_path);
+    }
+}
 
 sub play {
     my($self, $filename) = @_;
@@ -50,9 +64,15 @@ Plagger::Plugin::Notify::Audio - Notifies feed updates via audio file
 
 =head1 SYNOPSIS
 
+  # play single file when feeds are updated
   - module: Notify::Audio
     config:
       filename: /path/to/foo.wav
+
+  # play enclosures downloaded with Filter::FetchEnclosure
+  - module: Notify::Audio
+    config:
+      play_enclosures: 1
 
 =head1 DESCRIPTION
 
@@ -64,21 +84,12 @@ This plugin plays audio file when you've got feed updates.
 
 =item filename
 
-Audio filename to play. Required.
+Audio filename to play. Required, if you don't set I<play_enclosures>.
 
-=back
+=item play_enclosures
 
-=head1 TODO
-
-=over 4
-
-=item *
-
-Configurable audio name per feed.
-
-=item *
-
-Play enclosures?
+If set, it'll play local enclosure file which are downloaded via
+Filter::FetchEnclosure, if any.
 
 =back
 
--- lib/Plagger/Plugin/Notify/Audio/iTunesWin32.pm
+++ lib/Plagger/Plugin/Notify/Audio/iTunesWin32.pm
@@ -7,7 +7,7 @@ use Win32::OLE;
 sub play {
     my($self, $filename) = @_;
     $filename or return $self->log(error => "filename is not set");
-    
+
     my $itunes = Win32::OLE->new("iTunes.Application");
     $itunes->PlayFile($filename);
 }
