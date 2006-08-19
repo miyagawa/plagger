e133c61a504dc74dc064fe5ac582414b9f4607f1
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-08-19 08:41:40 +0000

    iTunesMac: sleep

--- lib/Plagger/Plugin/Notify/Audio/iTunesMac.pm
+++ lib/Plagger/Plugin/Notify/Audio/iTunesMac.pm
@@ -5,7 +5,7 @@ use base qw( Plagger::Plugin::Notify::Audio);
 use Mac::iTunes;
 
 sub play {
-    my($self, $filename) = @_;
+    my($self, $filename, $length) = @_;
 
     my $playlist = ref $self;
     my $itunes = Mac::iTunes->controller;
@@ -13,6 +13,7 @@ sub play {
     $itunes->delete_playlist($playlist);
     $itunes->add_track($filename, $playlist);
     $itunes->play_track(1, $playlist);
+    sleep $length;
 }
 
 1;
