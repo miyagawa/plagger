26e7731141def0b61e9eaf5239ba44e1788d674f
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-08-19 08:38:05 +0000

    add test file for Filter::2chRSSContent. Remove ad entry. Thanks to 33rpm

--- lib/Plagger/Plugin/Filter/2chRSSContent.pm
+++ lib/Plagger/Plugin/Filter/2chRSSContent.pm
@@ -18,7 +18,6 @@ sub filter {
     my $body = $args->{entry}->body;
     if ($body =~ s!^([^:]*):(\d{4}/\d\d/\d\d)\(.*?\) (\d\d:\d\d:\d\d)(?:\.\d\d)? (ID:\S+)?  ?!!) {
         my($from, $day, $time, $id) = ($1, $2, $3, $4);
-
         my $date = Plagger::Date->strptime('%Y/%m/%d %H:%M:%S', "$day $time");
         $date->set_time_zone('Asia/Tokyo');
 
@@ -27,7 +26,8 @@ sub filter {
         $args->{entry}->date($date);
         $args->{entry}->author( $from ? "$from $id" : $id );
         $args->{entry}->body($body);
-    } elsif ($args->{entry}->title =~ /^\d+\-$/) {
+    } elsif ($args->{entry}->title =~ /^\d+\-$/
+             || $args->{entry}->body =~ m!http://www\.2ch\.net/ad\.html *powerd by Big-Server\.!) {
         $context->log(info => "Strip 2ch bogus entry " . $args->{entry}->title);
         $args->{feed}->delete_entry($args->{entry});
     }
---
+++ t/plugins/Filter-2chRSSContent/base.t
@@ -0,0 +1,24 @@
+use strict;
+use t::TestPlagger;
+
+test_plugin_deps;
+plan 'no_plan';
+run_eval_expected;
+
+__END__
+
+=== Loading Filter::2chRSSContent
+--- input config
+plugins:
+  - module: Subscription::Config
+    config:
+      feed:
+        - file:///$FindBin::Bin/../../samples/top10news.xml
+
+  - module: Filter::2chRSSContent
+--- expected
+is $context->update->feeds->[0]->count, 1;
+ok $context->update->feeds->[0]->entries->[0]->date;
+ok $context->update->feeds->[0]->entries->[0]->author;
+ok ! grep { $_->title =~ /^\d+\-$/ }
+          $context->update->feeds->[0]->entries;
