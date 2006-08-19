be9acb76bd2b5e915ff46d7eed80382460c3a8b6
youpy <youpy@buycheapviagraonlinenow.com>
2006-08-19 08:37:09 +0000

    added Notify::Audio::iTunesMac

---
+++ lib/Plagger/Plugin/Notify/Audio/iTunesMac.pm
@@ -0,0 +1,18 @@
+package Plagger::Plugin::Notify::Audio::iTunesMac;
+use strict;
+use base qw( Plagger::Plugin::Notify::Audio);
+
+use Mac::iTunes;
+
+sub play {
+    my($self, $filename) = @_;
+
+    my $playlist = ref $self;
+    my $itunes = Mac::iTunes->controller;
+
+    $itunes->delete_playlist($playlist);
+    $itunes->add_track($filename, $playlist);
+    $itunes->play_track(1, $playlist);
+}
+
+1;
