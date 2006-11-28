eaf15dcef1ffec26362e3f54e80691c8bccca0c9
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-11-28 07:45:40 +0000

    rough cut of Filter::HTMLTidy

---
+++ lib/Plagger/Plugin/Filter/HTMLTidy.pm
@@ -0,0 +1,76 @@
+package Plagger::Plugin::Filter::HTMLTidy;
+use strict;
+use base qw( Plagger::Plugin );
+
+use HTML::Tidy;
+
+sub register {
+    my($self, $context) = @_;
+    $context->register_hook(
+        $self,
+        'update.entry.fixup' => \&filter,
+    );
+}
+
+our %defaults = (
+    output_xml   => 1,
+    wrap         => 0,
+    break_before_br => 0,
+    input_encoding => 'utf8',
+    output_encoding => 'utf8',
+);
+
+sub filter {
+    my($self, $context, $args) = @_;
+
+    my $body = $args->{entry}->body;
+    return unless $body && $body->is_html;
+
+    my $conf = $self->conf || {};
+    while (my($key, $value) = each %defaults) {
+        $conf->{$key} = $value unless exists $conf->{$key};
+    }
+
+    my $tidy = HTML::Tidy->new( $self->conf || {} );
+    $tidy->ignore( type => TIDY_WARNING );
+    my $new_body = $tidy->clean($body->data); # pass in Unicode string, not UTF-8
+
+    # HACK to extract <body /> only
+    $new_body =~ s!^.*<body>\s*(.*?)\s*</body>\s*</html>\s*$!$1!s;
+
+    $args->{entry}->body($new_body);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::Filter::HTMLTidy - Filters body HTML using HTML::Tidy
+
+=head1 SYNOPSIS
+
+  - module: Filter::HTMLTidy
+    config:
+      output-xhtml: yes
+      char-encoding: utf-8
+
+=head1 DESCRIPTION
+
+This plugin glues HTML::Tidy as an entry filter, so it scrubs HTML to
+make it tidy. Best used with Publish plugins like Planet.
+
+=head1 CONFIG
+
+This plugin accepts any config options that can be used as htmltidy
+config file.  See L<http://tidy.sourceforge.net/docs/quickref.html> for details.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<Plagger>, L<HTML::Tidy>, L<http://tidy.sourceforge.net/docs/quickref.html>
+
+=cut
---
+++ t/plugins/Filter-HTMLTidy/base.t
@@ -0,0 +1,100 @@
+use strict;
+use utf8;
+use t::TestPlagger;
+
+test_plugin_deps;
+
+filters 'chomp';
+filters { input => [ 'make_config', 'config', 'body', ] };
+filters_delay;
+
+my $tidy_config;
+
+sub make_config {
+    my $config = <<CONF;
+plugins:
+  - module: CustomFeed::Debug
+    config:
+      title: Foo
+      entry:
+        - title: Blah
+          body: $_[0]
+  - module: Filter::HTMLTidy
+    config:
+CONF
+    if ($tidy_config) {
+        $tidy_config =~ s/^/      /gm; # indent
+        $config .= $tidy_config . "\n";
+    }
+
+    $config;
+}
+
+sub body { $_[0]->update->feeds->[0]->entries->[0]->body->data }
+
+plan tests => 1 * blocks;
+
+for my $block (blocks) {
+    $tidy_config = $block->config;
+    $block->run_filters;
+    is $block->input, $block->expected, $block->name;
+};
+
+__END__
+
+=== Default
+--- input
+<p>Hello World</p>
+--- expected
+<p>Hello World</p>
+
+=== Text
+--- input
+This is a text, not HTML.
+--- expected
+This is a text, not HTML.
+
+=== Complex HTML
+--- input
+<p><div>fooo <br><font face="foo">foo</div></p><div><hr>foo<i>bar</i><p>aaa
+--- expected
+<div>fooo
+<br />
+<font face="foo">foo</font></div>
+<div>
+<hr />foo
+<i>bar</i>
+<p>aaa</p></div>
+
+=== html2xhtml
+--- input
+<p>foo<br>bar</p>
+--- expected
+<p>foo
+<br />bar</p>
+
+=== HTML chars
+--- input
+<p>You &amp; I</p>
+--- expected
+<p>You &amp; I</p>
+
+=== HTML special chars
+--- input
+<p>You &nbsp; I</p>
+--- expected
+<p>You &nbsp; I</p>
+
+=== HTML special chars
+--- input
+<p>You &aring; I</p>
+--- expected
+<p>You å I</p>
+
+=== UTF-8
+--- input
+<p>日本語のテスト <img src="foo.jpg" alt="顔"></p>
+--- expected
+<p>日本語のテスト 
+<img src="foo.jpg" alt="顔" /></p>
+
