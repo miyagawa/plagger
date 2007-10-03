d644161d6ae26d4cb27858d233d5e14bb485e097
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2007-10-03 07:35:26 +0000

    add psp store CF

---
+++ assets/plugins/CustomFeed-Script/jp-playstation-store.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use Web::Scraper;
+use URI;
+use YAML;
+
+my $uri = URI->new("http://www.jp.playstation.com/store/");
+my $scraper = scraper {
+    result->{link} = $uri; # xxx
+    process "title", title => 'TEXT';
+    process "#Sinfo p a", 'entries[]' => { link => '@href', title => 'TEXT' };
+};
+my $result = $scraper->scrape($uri);
+
+print Dump $result;
+
+
+
+
